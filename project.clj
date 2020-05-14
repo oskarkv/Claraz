f59fbd2c7f292f841bdf10016a0a8fceab89b448
Oskar Kvist <oskar.kvist@gmail.com>
2020-05-14 14:42:14 +0200

    Set version to 0.1.1-SNAPSHOT in project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject claraz/claraz "0.1.0"
+(defproject claraz/claraz "0.1.1-SNAPSHOT"
   :url "https://github.com/oskarkv/Claraz"
   :dependencies [[com.cerner/clara-rules "0.20.0"]]
   :source-paths ["src"]
