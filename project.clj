3899a381d4680a61a497d97c455995f4ccb02525
Oskar Kvist <oskar.kvist@gmail.com>
2020-05-14 12:25:42 +0200

    Add project.clj

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject claraz/claraz "0.1.0"
+  :url "https://github.com/oskarkv/Claraz"
+  :dependencies [[com.cerner/clara-rules "0.20.0"]]
+  :source-paths ["src"]
+  :test-paths ["test"]
+  :license {:name "MIT License"}
+  :repositories [["clojars" {:url "https://clojars.org/repo"
+                             :sign-releases false}]])
